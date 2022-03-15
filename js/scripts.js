1dcefa6696677651f4be510c0c23d8e94b1d507c
BrettHuang <Brett@EdokiAcademy.com>
2022-03-14 21:38:25 -0400

    Update template

--- js/scripts.js
+++ js/scripts.js
@@ -11,90 +11,136 @@
 */
 
 (function($) {
+  // Show current year
+  $('#current-year').text(new Date().getFullYear())
 
-    // Show current year
-    $("#current-year").text(new Date().getFullYear());
+  // Remove no-js class
+  $('html').removeClass('no-js')
 
-    // Remove no-js class
-    $('html').removeClass('no-js');
+  // Animate to section when nav is clicked
+  $('header a').click(function (e) {
+    // Treat as normal link if no-scroll class
+    if ($(this).hasClass('no-scroll')) return
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    e.preventDefault()
+    var heading = $(this).attr('href')
+    var scrollDistance = $(heading).offset().top
 
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+    $('html, body').animate(
+      {
+        scrollTop: scrollDistance + 'px',
+      },
+      Math.abs(window.pageYOffset - $(heading).offset().top) / 1
+    )
 
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+    // Hide the menu once clicked if mobile
+    if ($('header').hasClass('active')) {
+      $('header, body').removeClass('active')
+    }
+  })
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+  // Scroll to top
+  $('#to-top').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    )
+  })
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
-    });
+  // Scroll to first element
+  $('#lead-down span').click(function () {
+    var scrollDistance = $('#lead').next().offset().top
+    $('html, body').animate(
+      {
+        scrollTop: scrollDistance + 'px',
+      },
+      500
+    )
+  })
 
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-    });
+  // Create timeline
+  $('#experience-timeline').each(function () {
+    $this = $(this) // Store reference to this
+    $userContent = $this.children('div') // user content
 
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
-    });
+    // Create each timeline block
+    $userContent.each(function () {
+      $(this)
+        .addClass('vtimeline-content')
+        .wrap(
+          '<div class="vtimeline-point"><div class="vtimeline-block"></div></div>'
+        )
+    })
 
-    // Create timeline
-    $('#experience-timeline').each(function() {
+    // Add icons to each block
+    $this.find('.vtimeline-point').each(function () {
+      $(this).prepend(
+        '<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>'
+      )
+    })
 
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+    // Add dates to the timeline if exists
+    $this.find('.vtimeline-content').each(function () {
+      var date = $(this).data('date')
+      if (date) {
+        // Prepend if exists
+        $(this)
+          .parent()
+          .prepend('<span class="vtimeline-date">' + date + '</span>')
+      }
+    })
+  })
 
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
+  // Open mobile menu
+  $('#mobile-menu-open').click(function () {
+    $('header, body').addClass('active')
+  })
 
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
-        });
+  // Close mobile menu
+  $('#mobile-menu-close').click(function () {
+    $('header, body').removeClass('active')
+  })
 
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
+  // Load additional projects
+  $('#view-more-projects').click(function (e) {
+    e.preventDefault()
+    $(this).fadeOut(300, function () {
+      $('#more-projects').fadeIn(300)
+    })
+  })
 
-    });
+  // projecct image viewer
+  $('.project-image .image').click(function () {
+    $('#full-image').attr('src', $(this).attr('alt'))
+    $('#image-viewer').show()
+  })
 
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
-        $('header, body').addClass('active');
-    });
+  $('#image-viewer .close').click(function () {
+    $('#image-viewer').hide()
+  })
 
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
-        $('header, body').removeClass('active');
-    });
+  // projecct video viewer
+  $('.project-video .image').click(function () {
+    console.log('click')
+    $('#full-video').attr('src', $(this).attr('alt'))
+    $('#video-viewer').show()
+  })
 
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
-        e.preventDefault();
-        $(this).fadeOut(300, function() {
-            $('#more-projects').fadeIn(300);
-        });
-    });
+  $('#video-viewer .close').click(function () {
+    $('#video-viewer').hide()
+  })
 
+  // projecct glb viewer
+  $('.project-glb .image').click(function () {
+    console.log('click')
+    $('#full-glb').attr('src', $(this).attr('alt'))
+    $('#glb-viewer').show()
+  })
+
+  $('#glb-viewer .close').click(function () {
+    $('#glb-viewer').hide()
+  })
+  
 })(jQuery);
