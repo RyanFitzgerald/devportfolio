36a9b031727871f8369631d07ffc53aad14c4bad
hyeji <kkangji7239@gmail.com>
2022-11-24 14:51:32 +0900

    Template Change

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,125 @@
 var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
-const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
-const rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
 
-gulp.task('scripts', function() {
-    return gulp.src('./js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(babel({
-          presets: [['@babel/env', {modules:false}]]
-        }))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('./js'));
+// Set the banner content
+var banner = ['/*!\n',
+  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+  ' */\n',
+  ''
+].join('');
+
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function() {
+  return gulp.src('scss/resume.scss')
+    .pipe(sass())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('styles', function () {
-    return gulp.src('./scss/styles.scss')
-        .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+// Minify compiled CSS
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('css/resume.css')
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', function() {
-    gulp.watch('./js/scripts.js', gulp.series('scripts'));
-    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+// Minify custom JS
+gulp.task('minify-js', function() {
+  return gulp.src('js/resume.js')
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('js'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+// Copy vendor files from /node_modules into /vendor
+// NOTE: requires `npm install` before running!
+gulp.task('copy', function() {
+  gulp.src([
+      'node_modules/bootstrap/dist/**/*',
+      '!**/npm.js',
+      '!**/bootstrap-theme.*',
+      '!**/*.map'
+    ])
+    .pipe(gulp.dest('vendor/bootstrap'))
+
+  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+    .pipe(gulp.dest('vendor/jquery'))
+
+  gulp.src(['node_modules/jquery.easing/*.js'])
+    .pipe(gulp.dest('vendor/jquery-easing'))
+
+  gulp.src([
+      'node_modules/font-awesome/**',
+      '!node_modules/font-awesome/**/*.map',
+      '!node_modules/font-awesome/.npmignore',
+      '!node_modules/font-awesome/*.txt',
+      '!node_modules/font-awesome/*.md',
+      '!node_modules/font-awesome/*.json'
+    ])
+    .pipe(gulp.dest('vendor/font-awesome'))
+
+  gulp.src([
+      'node_modules/devicons/**/*',
+      '!node_modules/devicons/*.json',
+      '!node_modules/devicons/*.md',
+      '!node_modules/devicons/!PNG',
+      '!node_modules/devicons/!PNG/**/*',
+      '!node_modules/devicons/!SVG',
+      '!node_modules/devicons/!SVG/**/*'
+    ])
+    .pipe(gulp.dest('vendor/devicons'))
+
+  gulp.src(['node_modules/simple-line-icons/**/*', '!node_modules/simple-line-icons/*.json', '!node_modules/simple-line-icons/*.md'])
+    .pipe(gulp.dest('vendor/simple-line-icons'))
+})
+
+// Default task
+gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+  gulp.watch('scss/*.scss', ['sass']);
+  gulp.watch('css/*.css', ['minify-css']);
+  gulp.watch('js/*.js', ['minify-js']);
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('*.html', browserSync.reload);
+  gulp.watch('js/**/*.js', browserSync.reload);
 });
