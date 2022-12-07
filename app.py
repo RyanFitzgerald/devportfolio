f9e008ec90ab7929cbbe6996dc9aea953b2e6720
oskar-duda <o.duda@poczta.onet.pl>
2022-12-07 22:53:06 +0100

    fix(portfolio): simplify app file

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask, render_template
 app = Flask(__name__,
             static_url_path='',
@@ -10,7 +8,3 @@ app = Flask(__name__,
 @app.route("/")
 def home():
     return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
