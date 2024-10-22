d12675149fdc7b7ae331c8b8458842281a2c045c
Purushottam Thapa <purushottam.thapa@euformatics.com>
2024-10-23 00:13:30 +0300

    add profile picture

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
 const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));  // Use Dart Sass
 const wait = require('gulp-wait');
-const babel = require('gulp-babel');;
+const babel = require('gulp-babel');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 
+// Task to process scripts
 gulp.task('scripts', function() {
     return gulp.src('./js/scripts.js')
-        .pipe(plumber(plumber({
+        .pipe(plumber({
             errorHandler: function (err) {
                 console.log(err);
                 this.emit('end');
             }
-        })))
+        }))
         .pipe(babel({
-          presets: [['@babel/env', {modules:false}]]
+            presets: [['@babel/env', {modules:false}]]
         }))
         .pipe(uglify({
             output: {
@@ -28,10 +29,11 @@ gulp.task('scripts', function() {
         .pipe(browserSync.stream());  // Inject changes into browser
 });
 
+// Task to process styles (Sass)
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
-        .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(wait(250))  // Wait to ensure files are ready
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))  // Use Dart Sass
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());  // Inject changes into browser
 });
@@ -40,13 +42,14 @@ gulp.task('styles', function () {
 gulp.task('serve', function() {
     browserSync.init({
         server: {
-            baseDir: './'  // Serve files from 'public' folder
+            baseDir: './'  // Serve files from root folder
         },
         port: 3000  // Server will run on port 3000
     });
 
+    // Watch for changes in scripts, styles, and HTML files
     gulp.watch('./js/scripts.js', gulp.series('scripts'));
-    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+    gulp.watch('./scss/**/*.scss', gulp.series('styles'));  // Watch all .scss files in scss folder
     gulp.watch('./*.html').on('change', browserSync.reload);  // Reload on HTML changes
 });
 
