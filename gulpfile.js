569ce99ca12beb71804c6f62f7621df6650d0d06
Purushottam Thapa <purushottam.thapa@euformatics.com>
2024-10-22 12:26:17 +0300

    create first version

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sass = require('gulp-sass');
 const wait = require('gulp-wait');
 const babel = require('gulp-babel');;
 const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
 
 gulp.task('scripts', function() {
     return gulp.src('./js/scripts.js')
@@ -23,17 +24,31 @@ gulp.task('scripts', function() {
             }
         }))
         .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('./js'));
+        .pipe(gulp.dest('./js'))
+        .pipe(browserSync.stream());  // Inject changes into browser
 });
 
 gulp.task('styles', function () {
     return gulp.src('./scss/styles.scss')
         .pipe(wait(250))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());  // Inject changes into browser
 });
 
-gulp.task('watch', function() {
+// Task to serve the site with browser-sync
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'  // Serve files from 'public' folder
+        },
+        port: 3000  // Server will run on port 3000
+    });
+
     gulp.watch('./js/scripts.js', gulp.series('scripts'));
     gulp.watch('./scss/styles.scss', gulp.series('styles'));
+    gulp.watch('./*.html').on('change', browserSync.reload);  // Reload on HTML changes
 });
+
+// Default task to run both 'serve' and 'watch'
+gulp.task('default', gulp.series('serve'));
