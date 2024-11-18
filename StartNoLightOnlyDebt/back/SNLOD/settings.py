26a07d1ab13fb1c606ccbe1c81b267f6be5e674c
구민성 <babyms999@gmail.com>
2024-11-18 10:52:06 +0900

    회원가입 기능 구현 완료

--- StartNoLightOnlyDebt/back/SNLOD/settings.py
+++ StartNoLightOnlyDebt/back/SNLOD/settings.py
@@ -162,3 +162,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
