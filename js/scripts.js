493eeb1b0347a5f551ba535b20bb3f53ed002826
Chanteria Milner <chanteria.milner@gmail.com>
2025-04-10 23:27:56 -0500

    finished overhauling UI

--- js/scripts.js
+++ js/scripts.js
@@ -53,32 +53,6 @@
         }, 500);
     });
 
-    // Create timeline
-    $('#experience-timeline').each(function() {
-
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
-
-        // Create each timeline block
-        $userContent.each(function() {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
-        });
-
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-briefcase"></i></div>');
-        });
-
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
-            }
-        });
-
-    });
-
     // Open mobile menu
     $('#mobile-menu-open').click(function() {
         $('header, body').addClass('active');
