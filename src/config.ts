67a6111dc7f03a9d256b31281bf05e0b8397d60b
Ryan Fitzgerald <me@ryanfitzgerald.ca>
2025-07-26 00:15:16 -0400

    Update About section to utilize dynamic content from config and adjust styling for better responsiveness

--- src/config.ts
+++ src/config.ts
@@ -9,4 +9,6 @@ export const siteConfig = {
     twitter: "https://twitter.com/yourprofile",
     github: "https://github.com/RyanFitzgerald",
   },
+  aboutMe:
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
 };
