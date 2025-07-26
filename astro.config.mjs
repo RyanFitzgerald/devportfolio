58e0448f2339b0658664aa839915f1ee83e56a23
Ryan Fitzgerald <me@ryanfitzgerald.ca>
2025-07-26 14:53:27 -0400

    Add GitHub pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ryanfitzgerald.github.io",
+  base: "devportfolio",
   vite: {
     plugins: [tailwindcss()],
   },
